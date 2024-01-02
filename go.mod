8033d7eb2fb57560859e6d9a0c4994ca80a1d9b1
matehaxor03 <matehaxor03>
2024-01-02 13:27:06 +1100

    create ramdisk

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/matehaxor03/holistic_install
 
 go 1.19
+
+require github.com/matehaxor03/holistic_host_client v0.0.1
+
+require (
+	github.com/matehaxor03/holistic_common v0.0.66 // indirect
+	github.com/matehaxor03/holistic_validator v0.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"os"
+	host_client "github.com/matehaxor03/holistic_host_client/host_client"
+
 )
 
 func main(){
-	os.Exit(1)
+	host_client, host_client_errors := host_client.NewHostClient()
+	if host_client_errors != nil {
+		os.Exit(1)
+	}
+
+	
+	ramdisk, ramdisk_errors := host_client.Ramdisk("ramdisk", uint64(2048*1000))
+	if ramdisk_errors != nil {
+		os.Exit(1)
+	}
+
+	ramdisk_create_errors := ramdisk.Create()
+	if ramdisk_create_errors != nil {
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
