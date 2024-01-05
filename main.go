77f89953ab02a6f2b89aa464e51ac7851bf8e83f
matehaxor03 <matehaxor03>
2024-01-06 10:24:12 +1100

    add error messages to console

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"fmt"
 	host_client "github.com/matehaxor03/holistic_host_client/host_client"
 
 )
@@ -9,19 +10,22 @@ import (
 func main(){
 	host_client, host_client_errors := host_client.NewHostClient()
 	if host_client_errors != nil {
+		fmt.Println(fmt.Errorf("%s", host_client_errors))
 		os.Exit(1)
 	}
 
-	
 	ramdisk, ramdisk_errors := host_client.Ramdisk("ramdisk", uint64(2048*1000))
 	if ramdisk_errors != nil {
+		fmt.Println(fmt.Errorf("%s", ramdisk_errors))
 		os.Exit(1)
 	}
 
 	ramdisk_create_errors := ramdisk.Create()
 	if ramdisk_create_errors != nil {
+		fmt.Println(fmt.Errorf("%s", ramdisk_create_errors))
 		os.Exit(1)
 	}
 
+	fmt.Println("install successfull")
 	os.Exit(0)
 }
