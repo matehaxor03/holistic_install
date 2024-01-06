e82f730265ca3cb7efb94e00b36dfac2f1580222
matehaxor03 <matehaxor03>
2024-01-06 12:18:18 +1100

    create databse as part of install

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/matehaxor03/holistic_install
 
 go 1.19
 
-require github.com/matehaxor03/holistic_host_client v0.0.4
+require (
+	github.com/matehaxor03/holistic_common v0.0.69
+	github.com/matehaxor03/holistic_db_init v0.0.1
+	github.com/matehaxor03/holistic_host_client v0.0.4
+)
 
 require (
-	github.com/matehaxor03/holistic_common v0.0.69 // indirect
+	github.com/matehaxor03/holistic_db_client v0.0.555 // indirect
+	github.com/matehaxor03/holistic_json v0.0.92 // indirect
 	github.com/matehaxor03/holistic_validator v0.0.8 // indirect
 )
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"os"
 	"fmt"
 	host_client "github.com/matehaxor03/holistic_host_client/host_client"
-
+	common "github.com/matehaxor03/holistic_common/common"
+	db_installer "github.com/matehaxor03/holistic_db_init/db_installer"
 )
 
 func main(){
+	var errors []error
 	host_client, host_client_errors := host_client.NewHostClient()
 	if host_client_errors != nil {
 		fmt.Println(fmt.Errorf("%s", host_client_errors))
@@ -23,6 +25,48 @@ func main(){
 	ramdisk_create_errors := ramdisk.Create()
 	if ramdisk_create_errors != nil {
 		fmt.Println(fmt.Errorf("%s", ramdisk_create_errors))
+		os.Exit(1)
+	}
+
+	database_host_name, database_host_name_errors := host_client.GetEnviornmentVariable(common.ENV_HOLISTIC_DATABASE_HOSTNAME())
+	if database_host_name_errors != nil {
+		errors = append(errors, database_host_name_errors...)
+	}
+
+	database_port_number, database_port_number_errors := host_client.GetEnviornmentVariable(common.ENV_HOLISTIC_DATABASE_PORT_NUMBER())
+	if database_port_number_errors != nil {
+		errors = append(errors, database_port_number_errors...)
+	}
+
+	database_name, database_name_errors := host_client.GetEnviornmentVariable(common.ENV_HOLISTIC_DATABASE_NAME())
+	if database_name_errors != nil {
+		errors = append(errors, database_name_errors...)
+	}
+
+	database_root_username, database_root_username_errors := host_client.GetEnviornmentVariable(common.ENV_HOLISTIC_DATABASE_ROOT_USERNAME())
+	if database_root_username_errors != nil {
+		errors = append(errors, database_root_username_errors...)
+	}
+
+	database_root_password, database_root_password_errors := host_client.GetEnviornmentVariable(common.ENV_HOLISTIC_DATABASE_ROOT_PASSWORD())
+	if database_root_password_errors != nil {
+		errors = append(errors, database_root_password_errors...)
+	}
+
+	if len(errors) > 0 {
+		fmt.Println(fmt.Errorf("%s", errors))
+		os.Exit(1)
+	}
+
+	database_installer,  database_installer_errors := db_installer.NewDatabaseInstaller(*database_host_name, *database_port_number, *database_name, *database_root_username, *database_root_password)
+	if database_installer_errors != nil {
+		fmt.Println(fmt.Errorf("%s", database_installer_errors))
+		os.Exit(1)
+	}
+
+	install_errors := database_installer.Install()
+	if install_errors != nil {
+		fmt.Println(fmt.Errorf("%s", install_errors))
 		os.Exit(1)
 	}
 
