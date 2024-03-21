c9fb6f7c6bfd57532f56e2c42e1899766d9c8570
matehaxor03 <matehaxor03>
2024-03-22 05:27:49 +1100

    wip install

--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/matehaxor03/holistic_install
 go 1.19
 
 require (
-	github.com/matehaxor03/holistic_common v0.0.69
-	github.com/matehaxor03/holistic_db_init v0.0.1
-	github.com/matehaxor03/holistic_host_client v0.0.4
+	github.com/matehaxor03/holistic_common v0.0.72
+	github.com/matehaxor03/holistic_db_init v0.0.2
+	github.com/matehaxor03/holistic_host_client v0.0.66
+	github.com/matehaxor03/holistic_host_init v0.0.1
 )
 
 require (
 	github.com/matehaxor03/holistic_db_client v0.0.555 // indirect
 	github.com/matehaxor03/holistic_json v0.0.92 // indirect
-	github.com/matehaxor03/holistic_validator v0.0.8 // indirect
+	github.com/matehaxor03/holistic_validator v0.0.9 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	host_client "github.com/matehaxor03/holistic_host_client/host_client"
 	common "github.com/matehaxor03/holistic_common/common"
+	host_installer "github.com/matehaxor03/holistic_host_init/host_installer"
 	db_installer "github.com/matehaxor03/holistic_db_init/db_installer"
 )
 
@@ -16,16 +17,14 @@ func main(){
 		os.Exit(1)
 	}
 
-	ramdisk, ramdisk_errors := host_client.Ramdisk("ramdisk", uint64(2048*1000))
-	if ramdisk_errors != nil {
-		fmt.Println(fmt.Errorf("%s", ramdisk_errors))
-		os.Exit(1)
+	number_of_users_value, number_of_users_errors := host_client.GetEnviornmentVariableValue(common.ENV_HOLISTIC_HOST_NUMBER_OF_USERS())
+	if number_of_users_errors != nil {
+		errors = append(errors, number_of_users_errors...)
 	}
 
-	ramdisk_create_errors := ramdisk.Create()
-	if ramdisk_create_errors != nil {
-		fmt.Println(fmt.Errorf("%s", ramdisk_create_errors))
-		os.Exit(1)
+	users_offset_value, users_offset_value_errors := host_client.GetEnviornmentVariableValue(common.ENV_HOLISTIC_HOST_USERS_USERID_OFFSET())
+	if users_offset_value_errors != nil {
+		errors = append(errors, users_offset_value_errors...)
 	}
 
 	database_host_name, database_host_name_errors := host_client.GetEnviornmentVariable(common.ENV_HOLISTIC_DATABASE_HOSTNAME())
@@ -58,15 +57,45 @@ func main(){
 		os.Exit(1)
 	}
 
-	database_installer,  database_installer_errors := db_installer.NewDatabaseInstaller(*database_host_name, *database_port_number, *database_name, *database_root_username, *database_root_password)
-	if database_installer_errors != nil {
-		fmt.Println(fmt.Errorf("%s", database_installer_errors))
+	number_of_users, number_of_users_uint64_errors := number_of_users_value.GetUInt64Value()
+	if number_of_users_uint64_errors != nil {
+		errors = append(errors, number_of_users_uint64_errors...)
+	}
+
+	userid_offset, userid_offset_uint64_errors := users_offset_value.GetUInt64Value()
+	if userid_offset_uint64_errors != nil {
+		errors = append(errors, userid_offset_uint64_errors...)
+	}
+
+	if len(errors) > 0 {
+		fmt.Println(fmt.Errorf("%s", errors))
 		os.Exit(1)
 	}
 
-	install_errors := database_installer.Install()
-	if install_errors != nil {
-		fmt.Println(fmt.Errorf("%s", install_errors))
+	host_installer, host_installer_errors := host_installer.NewHostInstaller(number_of_users, userid_offset)
+	if host_installer_errors != nil {
+		errors = append(errors, host_installer_errors...)
+	}
+
+	database_installer,  database_installer_errors := db_installer.NewDatabaseInstaller(*database_host_name, *database_port_number, *database_name, *database_root_username, *database_root_password)
+	if database_installer_errors != nil {
+		errors = append(errors, database_installer_errors...)
+	}
+
+	if len(errors) > 0 {
+		fmt.Println(fmt.Errorf("%s", errors))
+		os.Exit(1)
+	}
+
+	host_installer_install_errors := host_installer.Install()
+	if host_installer_install_errors != nil {
+		fmt.Println(fmt.Errorf("%s", host_installer_install_errors))
+		os.Exit(1)
+	}
+
+	database_installer_install_errors := database_installer.Install()
+	if database_installer_install_errors != nil {
+		fmt.Println(fmt.Errorf("%s", database_installer_install_errors))
 		os.Exit(1)
 	}
 
